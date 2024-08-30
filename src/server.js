03fdf23d084bd1786a3402adb23f76ee4223d393
e2e2a <emonawong22@gmail.com>
2024-08-30 17:08:11 +0800

    add

--- src/server.js
+++ src/server.js
@@ -3,19 +3,19 @@ import next from 'next';
 import { Server } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
-
 app.prepare().then(() => {
   const httpServer = createServer(handler);
 
   const io = new Server(httpServer,{
     path: '/socket.io', // Ensure this path matches your client-side connection
     cors: {
-      origin: '*',
+      // origin: '*',
+      origin: 'https://nextjs-enrollment.vercel.app',
       methods: ['GET', 'POST']
     }
   }
