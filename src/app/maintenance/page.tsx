058dd431fdde0b1d116ad52cf9098114ebc59a6e
e2e2a <emonawong22@gmail.com>
2024-09-21 20:58:56 +0800

    add

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -10,7 +10,7 @@ export default function MaintenancePage() {
       <div className='text-center'>
         <div className='flex items-center justify-center'>
           {/* <img src='/images/logo1.png' alt='Logo' className='h-32 w-32 rounded-full' /> */}
-          <Image src={'/images/logo1.png'} alt='nothing to say' width={100} height={100} priority className='rounded-full ' />
+          <Image src={'https://firebasestorage.googleapis.com/v0/b/my-project-eaeb4.appspot.com/o/logo%2Flogo1.png?alt=media&token=8ebc5ba9-67b3-47fa-ae2c-7cb1e7d9c76f'} alt='nothing to say' width={100} height={100} priority className='rounded-full ' />
         </div>
         <h1 className=' text-xl xs:text-2xl sm:text-3xl md:text-5xl font-bold text-gray-800 mb-4'>We&apos;re Under Maintenance</h1>
         <p className='sm:text-lg text-sm text-gray-600 mb-6'>Sorry for the inconvenience. Our site is currently undergoing scheduled maintenance.</p>
