8cf4ce307a5ddf6ae8306660ea5baa0d6d6cfae0
e2e2a <emonawong22@gmail.com>
2024-09-25 20:30:29 +0800

    add

--- src/components/shared/nav/LogoutButton.tsx
+++ src/components/shared/nav/LogoutButton.tsx
@@ -7,7 +7,7 @@ import { updateUserLogout } from '@/services/user';
 
 const LogoutButton = () => {
   return (
-    <Button type='button' onClick={async() => await signOut()} className='group select-none border-0 w-full hover:bg-slate-300 hover:bg-opacity-70 px-5 py-6 flex space-x-2 items-center gap-x-1 justify-start pl-3'>
+    <Button type='button' onClick={() => signOut({callbackUrl: process.env.NEXT_PUBLIC_APP_URL})} className='group select-none border-0 w-full hover:bg-slate-300 hover:bg-opacity-70 px-5 py-6 flex space-x-2 items-center gap-x-1 justify-start pl-3'>
       <Icons.logout className='h-7 w-7 group-hover:stroke-blue-500' />
       <span className='font-semibold text-sm tracking-tight'>Sign out</span>
     </Button>
