bc203f3d4d9b50c928604a0adde77649d6f34d87
e2e2a <emonawong22@gmail.com>
2024-09-25 21:28:38 +0800

    add

--- src/components/shared/nav/LogoutButton.tsx
+++ src/components/shared/nav/LogoutButton.tsx
@@ -1,13 +1,19 @@
-
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import { Button } from '../../ui/button';
 import { Icons } from '../Icons';
 import { signOut } from 'next-auth/react';
 import { updateUserLogout } from '@/services/user';
+import { useRouter } from 'next/navigation';
 
 const LogoutButton = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    signOut();
+    router.push('/sign-in');
+  };
   return (
-    <Button type='button' onClick={() => signOut({callbackUrl: process.env.NEXT_PUBLIC_APP_URL})} className='group select-none border-0 w-full hover:bg-slate-300 hover:bg-opacity-70 px-5 py-6 flex space-x-2 items-center gap-x-1 justify-start pl-3'>
+    <Button type='button' onClick={handleClick} className='group select-none border-0 w-full hover:bg-slate-300 hover:bg-opacity-70 px-5 py-6 flex space-x-2 items-center gap-x-1 justify-start pl-3'>
       <Icons.logout className='h-7 w-7 group-hover:stroke-blue-500' />
       <span className='font-semibold text-sm tracking-tight'>Sign out</span>
     </Button>
