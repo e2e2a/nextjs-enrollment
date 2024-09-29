9bb3240f069bcd4e4be2f102622c051b99974ec7
e2e2a <emonawong22@gmail.com>
2024-09-29 09:53:35 +0800

    add

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ const nextConfig = {
   //   compiler: {
   //     removeConsole: true,
   //   },
+  // Customize Webpack configuration
+  // webpack: (config, { isServer }) => {
+  //   // Example: Aliases
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@components': path.resolve(__dirname, 'src/components'),
+  //   };
+
+  //   // Important: return the modified config
+  //   return config;
+  // },
+  // swcMinify: true,
+  // experimental: {
+  //   optimizeServerReact: true,
+  //   cpus: 15,
+  //   optimisticClientCache: true,
+  // },
+
+
+
   async redirects() {
     const isInMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === '1';
 
