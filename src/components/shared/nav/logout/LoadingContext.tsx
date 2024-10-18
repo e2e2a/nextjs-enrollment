2518788f3919b6a853d5f0a84e5f68a6b4c6b960
e2e2a <emonawong22@gmail.com>
2024-10-18 16:22:51 +0800

    Add

--- src/components/shared/nav/logout/LoadingContext.tsx
+++ src/components/shared/nav/logout/LoadingContext.tsx
@@ -24,23 +24,47 @@ export const LoadingProvider: React.FC<{ children: React.ReactNode }> = ({ child
 const LoadingOverlay = () => {
   const flyVariants = {
     animate: {
-      x: ['100vw', '0vw', '0vw', '-100vw'], // Start off-screen (right), move to center, stay, then off-screen (left)
-      rotateY: [0, 0, 180, 0], // Rotate around Y-axis while paused in the center
+      y: ['-100vh', '0vh'], // Start off-screen (top) and move to center
+      rotateY: [0, 360], // Rotate continuously after reaching the center
       transition: {
-        duration: 5, // Total time for the entire cycle (1 sec to center + 3 sec pause + 1 sec to exit)
-        times: [0, 0.2, 0.8, 1], // Control the timing of keyframes
+        duration: 1, // 1 sec to move to the center, then rotation continues
+        times: [0, 1], // The movement happens only once
         ease: 'easeInOut', // Smooth transition
-        repeat: Infinity, // Repeat animation indefinitely
+        rotateY: {
+          delay: 1, // Delay the rotation to start after the movement
+          duration: 5, // How long each full rotation takes
+          repeat: Infinity, // Rotate continuously once in the center
+          ease: 'linear', // Keep the rotation constant and smooth
+        },
       },
     },
   };
+
   return (
-    <div className='fixed inset-0 flex justify-center top-0 items-center bg-white z-50 select-none'>
+    <div className='fixed inset-0 flex justify-center items-center bg-white z-50 select-none'>
       <div className='flex flex-col items-center'>
         <motion.div variants={flyVariants} animate='animate'>
           <Image src='/images/logo1.png' alt='loader' width={150} height={150} priority />
         </motion.div>
-        <p className='mt-4 text-gray-700'>Signing out...</p>
+        <style jsx>{`
+          .pulse-gradient {
+            background-size: 200% 200%;
+            animation: pulseGradient 3s ease infinite;
+          }
+
+          @keyframes pulseGradient {
+            0% {
+              background-position: 0% 50%;
+            }
+            50% {
+              background-position: 100% 50%;
+            }
+            100% {
+              background-position: 0% 50%;
+            }
+          }
+        `}</style>
+        <p className='mt-4 text-transparent font-bold sm:font-semibold uppercase text-lg sm:text-xl md:text-2xl bg-gradient-to-r bg-clip-text pulse-gradient from-purple-400 via-pink-500 to-red-500'>Signing out</p>
       </div>
     </div>
   );
