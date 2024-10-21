8cf890d796464a108902e9adbbbaa8446ea449b1
e2e2a <emonawong22@gmail.com>
2024-10-21 11:11:37 +0800

    add

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,13 +1,7 @@
 'use client';
-import Loader from '@/components/shared/Loader';
-import { MainNav } from '@/components/shared/nav/MainNav';
-import { MobileNav } from '@/components/shared/nav/MobileNav';
-import { SidebarNav } from '@/components/shared/nav/SidebarNav';
-import { dashboardConfig } from '@/constant/dashboard';
-import { useProfileQuery } from '@/lib/queries';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { data: sessionData } = useSession();
@@ -15,9 +9,9 @@ const Layout = ({ children }: { children: ReactNode }) => {
   if (!sessionData) {
     return redirect('/sign-in');
   }
-//   if (sessionData && !sessionData.user.profileVerified) {
-//     return redirect('/profile');
-//   }
+  //   if (sessionData && !sessionData.user.profileVerified) {
+  //     return redirect('/profile');
+  //   }
 
   return <div>{children}</div>;
 };
