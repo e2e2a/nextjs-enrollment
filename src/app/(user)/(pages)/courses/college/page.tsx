0aa27fd20e2ef48ceb1be7c636b0a41cbab55a15
e2e2a <emonawong22@gmail.com>
2024-11-22 04:17:56 +0800

    added links to courses to navigate the curriculums of course
    Signed-off-by: e2e2a <emonawong22@gmail.com>

--- src/app/(user)/(pages)/courses/college/page.tsx
+++ src/app/(user)/(pages)/courses/college/page.tsx
@@ -10,16 +10,14 @@ import { useCourseQueryByCategory } from '@/lib/queries/courses/get/category';
 const Page = () => {
   const [isPageLoading, setIsPageLoading] = useState(true);
   const { data: c, isLoading: cLoading, error: cError } = useCourseQueryByCategory('College');
+
   useEffect(() => {
-    if (cLoading || !c) {
-      return;
-    }
-    if (cError) return; //setError(true);
+    if (cError || !c) return;
 
     if (c) {
       setIsPageLoading(false);
     }
-  }, [c, cLoading, cError]);
+  }, [c, cError]);
 
   return (
     <>
@@ -32,7 +30,6 @@ const Page = () => {
             <div className='flex w-full flex-col '>
               <h1 className='font-semibold tracking-tight text-3xl lg:text-left text-center w-full'>Available College Courses</h1>
             </div>
-            {/* <div className='grid lg:grid-cols-2 shadow-none drop-shadow-none xl:grid-cols-3 gap-y-8 sm:grid-cols-2 grid-cols-1 gap-4 my-5 w-full place-items-center bg-transparent'> */}
             <div className='grid gap-y-8 sm:grid-cols-1 grid-cols-1 gap-4 my-5 w-full place-items-center bg-transparent'>
               {c?.courses?.map((course: any, index: any) => (
                 <Card className='w-full group shadow-sm drop-shadow-sm bg-white' key={index}>
@@ -44,15 +41,20 @@ const Page = () => {
                       <div className='w-full sm:w-56 p-5 '>
                         <Image src={`${course.imageUrl ? course.imageUrl : ''}`} className='w-full sm:w-56 border rounded-lg sm:h-48 lg:h-44 bg-white group-hover:border-blue-200' width={200} height={200} priority alt={'nothing to say'} />
                       </div>
-                      <div className='flex flex-col flex-1 p-5'>
-                        <div className='flex w-full justify-end items-center hover:underline'>
-                          <Link href={`/enrollment/college?courses=${course.courseCode}`} className='flex gap-2 items-center text-sm'>
+                      <div className='flex justify-between flex-1 p-5'>
+                        <div className='flex flex-col w-full justify-start'>
+                          <Link href={`/curriculums/${course.courseCode}`} className='flex gap-2 items-center text-sm hover:underline'>
+                            <span className='text-blue-700'>View Curriculum</span> <Icons.graduationCap className='h-5 w-5 stroke-orange-300' />
+                          </Link>
+                          <h1 className=' text-xl tracking-tight capitalize font-semibold'>{course.name}</h1>
+                          <div className='space-y-1.5 flex flex-col px-2'>
+                            <p className='text-sm text-muted-foreground text-justify first-letter:uppercase'>&nbsp;&nbsp;&nbsp;&nbsp;{course.description}</p>
+                          </div>
+                        </div>
+                        <div className='flex w-full justify-end items-start'>
+                          <Link href={`/enrollment/college?courses=${course.courseCode}`} className='flex gap-2 items-center text-sm hover:underline'>
                             <span className='text-blue-700'>Enroll</span> <Icons.arrowRight className='h-4 w-4' />
                           </Link>
-                        </div>
-                        <h1 className=' text-xl tracking-tight capitalize font-semibold'>{course.name}</h1>
-                        <div className='space-y-1.5 flex flex-col px-2'>
-                          <p className='text-sm text-muted-foreground text-justify first-letter:uppercase'>&nbsp;&nbsp;&nbsp;&nbsp;{course.description}</p>
                         </div>
                       </div>
                     </div>
