82af756f7b16fd571f492d1a35ce1c5ed19ba170
raymond.g@techguys.work <raymond.g@techguys.work>
2025-01-23 23:15:05 +0800

    add

--- src/action/courses/update/id/index.ts
+++ src/action/courses/update/id/index.ts
@@ -4,8 +4,7 @@ import dbConnect from '@/lib/db/db';
 import { tryCatch } from '@/lib/helpers/tryCatch';
 import { CourseValidatorInCollege } from '@/lib/validators/course/create/college';
 import Course from '@/models/Course';
-import { createCourse, getCourseByCourseCode, getCourseByName, getCoursesById } from '@/services/course';
-import { createCurriculum } from '@/services/curriculum';
+import { getCourseByCourseCode, getCourseByName, getCoursesById } from '@/services/course';
 import { verifyADMIN } from '@/utils/actions/session/roles/admin';
 import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
@@ -21,32 +20,37 @@ export const updateCourseByIdAction = async (data: any) => {
     const session = await verifyADMIN();
     if (!session || session.error) return { error: 'Not Authorized.', status: 403 };
 
-    const { formData, ...remainData } = data;
-    const courseParse = CourseValidatorInCollege.safeParse(remainData);
-    if (!courseParse.success) return { error: 'Invalid fields!', status: 400 };
+    const a = await checkCategory(session.user, data)
 
-    const course = await getCoursesById(data.id);
-    if (!course) return { error: 'Course not found.', status: 404 };
-    if (course.courseCode.toLowerCase() !== data.courseCode.toLowerCase()) {
-      const checkCourseCode = await getCourseByCourseCode(data.courseCode);
-      if (checkCourseCode) return { error: 'Course code already exist, please choose another code.', status: 409 };
+    return a
+  });
+};
+
+/**
+ * check category
+ *
+ * @param {object} user
+ * @param {object} data
+ */
+const checkCategory = async (user: any, data: any) => {
+  return tryCatch(async () => {
+    let category;
+    switch (data.category) {
+      case 'College':
+        category = await categoryCollege(user, data);
+        break;
+      default:
+        return { error: 'Invalid category.', status: 400 };
     }
 
-    if (course.name.toLowerCase() !== data.name.toLowerCase()) {
-      const checkName = await getCourseByName(data.name);
-      if (checkName) return { error: 'Course name already exist, please choose another name.', status: 409 };
-    }
-
-    const a = await checkPhotoAndStore(formData, courseParse.data, data.id);
-
-    return a;
+    return category;
   });
 };
 
 /**
  * check photo and save to firebase
  *
- * @param {any} formData
+ * @param {File} formData
  * @param {object} data
  */
 export const checkPhotoAndStore = async (formData: any, data: any, id: string) => {
@@ -58,14 +62,12 @@ export const checkPhotoAndStore = async (formData: any, data: any, id: string) =
       const image = formData.get('image') as File;
       if (image && image.name) {
         if (cc.imageUrl) {
-          console.log('running');
           // Extract the path of the old image from the URL
           const oldImagePath = cc.imageUrl.replace(/.*\/o\/(.+?)\?.*/, '$1'); // Decode Firebase Storage URL
           const oldImageRef = ref(storage, decodeURIComponent(oldImagePath));
 
           // Delete the old image
           await deleteObject(oldImageRef);
-          console.log('Old image deleted successfully.');
         }
         const storageRef = ref(storage, `courses/${cc._id}/${image.name}`);
         await uploadBytes(storageRef, image, { contentType: image.type });
@@ -80,3 +82,28 @@ export const checkPhotoAndStore = async (formData: any, data: any, id: string) =
     return { success: true, message: 'New Course has been added.', category: data.category, status: 201 };
   });
 };
+
+const categoryCollege = async (user: any, data: any)=>{
+  return tryCatch(async () => {
+    const { formData, ...remainData } = data;
+    const courseParse = CourseValidatorInCollege.safeParse(remainData);
+    if (!courseParse.success) return { error: 'Invalid fields!', status: 400 };
+
+    const course = await getCoursesById(data.id);
+    if (!course) return { error: 'Course not found.', status: 404 };
+
+    if (course.courseCode.toLowerCase() !== data.courseCode.toLowerCase()) {
+      const checkCourseCode = await getCourseByCourseCode(data.courseCode);
+      if (checkCourseCode) return { error: 'Course code already exist, please choose another code.', status: 409 };
+    }
+
+    if (course.name.toLowerCase() !== data.name.toLowerCase()) {
+      const checkName = await getCourseByName(data.name);
+      if (checkName) return { error: 'Course name already exist, please choose another name.', status: 409 };
+    }
+
+    const a = await checkPhotoAndStore(formData, courseParse.data, data.id);
+
+    return a;
+  });
+}
