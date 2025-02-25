fe85d4d78a9c8b789bb7dc43e53c2f55348fa871
e2e2a <emonawong22@gmail.com>
2025-02-25 14:01:44 +0800

    creation of blocks getting duplicate

--- src/action/blocks/create/admin.ts
+++ src/action/blocks/create/admin.ts
@@ -4,6 +4,8 @@ import { tryCatch } from '@/lib/helpers/tryCatch';
 import { BlockValidatorInCollege } from '@/lib/validators/block/create/college';
 import { createBlockType, getfilterBlockType } from '@/services/blockType';
 import { getCourseByCourseCode } from '@/services/course';
+import { getDeanProfileByUserId } from '@/services/deanProfile';
+import { checkAuth } from '@/utils/actions/session';
 
 /**
  * check role
@@ -14,12 +16,13 @@ import { getCourseByCourseCode } from '@/services/course';
 export const createCollegeCourseBlockAction = async (data: any) => {
   return tryCatch(async () => {
     await dbConnect();
-    // must have a condition for admin roles
-    // for now empty
+    const session = await checkAuth();
+    if (!session || session.error) return { error: 'Not authenticated.', status: 403 };
+
     const blockParse = BlockValidatorInCollege.safeParse(data);
     if (!blockParse.success) return { error: 'Invalid fields!', status: 400 };
 
-    const checkedBlock = await checkBlockType(blockParse.data);
+    const checkedBlock = await checkBlockType(session.user, blockParse.data);
 
     return checkedBlock;
   });
@@ -30,12 +33,22 @@ export const createCollegeCourseBlockAction = async (data: any) => {
  *
  * @param {object} data
  */
-const checkBlockType = async (data: any) => {
+const checkBlockType = async (user: any, data: any) => {
   return tryCatch(async () => {
-    const checkC = await getCourseByCourseCode(data.courseCode);
-    if (!checkC) return { error: 'Course not found.', status: 404 };
-    delete data.courseCode;
-    data.courseId = checkC._id;
+    let category = '';
+    if (user.role === 'ADMIN') {
+      const checkC = await getCourseByCourseCode(data.courseCode);
+      if (!checkC) return { error: 'Course not found.', status: 404 };
+      delete data.courseCode;
+      data.courseId = checkC._id;
+      category = checkC.category;
+    }
+
+    if (user.role === 'DEAN') {
+      const a = await getDeanProfileByUserId(user._id);
+      data.courseId = a.courseId._id;
+      category = a.courseId.category;
+    }
 
     const checkB = await getfilterBlockType(data);
     if (checkB) return { error: `${data.year}-${data.semester}-Block ${data.section} is already exist in the course.`, status: 500 };
@@ -43,6 +56,6 @@ const checkBlockType = async (data: any) => {
     const p = await createBlockType(data);
     if (!p) return { error: 'Something went wrong in creating block.', status: 500 };
 
-    return { success: true, message: 'Block Type created successfully created', courseId: checkC._id.toString(), category: checkC.category, status: 201 };
+    return { success: true, message: 'Block Type created successfully created', courseId: data.courseId._id.toString(), category: category, status: 201 };
   });
 };
